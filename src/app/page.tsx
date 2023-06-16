f9e95d6f4543c97b101f0fe78f97d0a86fd4d224
alaqsaka <aqsaabidin15@gmail.com>
2023-06-16 23:46:21 +0700

    force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import { useRouter, useSearchParams } from "next/navigation";
 import PokemonCard from "./components/Pokemon/PokemonCard";
 import { Pokemon } from "./types";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const searchParams = useSearchParams();
   const router = useRouter();
